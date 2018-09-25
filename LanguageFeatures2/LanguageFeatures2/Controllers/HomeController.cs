9d972ecddac5f46af9dafe9eb30ee5bb2c4f5b37
Yaroslav Maslay <yaroslavmaslay@gmail.com>
2018-09-25 11:59:28 +0300

    First Push

---
+++ LanguageFeatures2/LanguageFeatures2/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using LanguageFeatures2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace LanguageFeatures2.Controllers
+{
+    public class HomeController: Controller
+    {
+        public ViewResult Index()
+        {
+            List<string> productList = new List<string>();
+
+            foreach (Product p in Product.GetProducts())
+            {
+                productList.Add($"Name:{p?.Name ?? "<no name>"}, Price:{p?.Price ?? 0}, RelatedName:{p?.Related?.Name?? "<no name>"}, {p?.Category}");
+            }
+
+            return View(productList);
+        }
+    }
+}
---
+++ LanguageFeatures2/LanguageFeatures2/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanguageFeatures2.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal? Price { get; set; }
+        public string Category { get; set; } = "Watersports";
+
+        public Product Related{ get; set; }
+        public static Product [] GetProducts()
+        {
+            Product kayak = new Product { Name = "Kayak", Price = 275M, Category="Water Craft" };
+            Product lifeJacket = new Product { Name = "LifeJacket", Price = 322M };
+            kayak.Related = lifeJacket;
+            return new Product[] { kayak, lifeJacket,null };
+        }
+    }
+}
